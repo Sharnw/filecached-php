3ea962a8262a31bb28ec2ac32971849ba3b6cce3
sharnw <sharn.white@gmail.com>
2016-12-07 13:23:13 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Filecached - PHP datastore #
+
+A simple key-value datastore using filesystem instead of memory.
+
+Useful for caching large datasets, and replacing memcache on low memory AWS instances.
+
+Credit to https://github.com/hustcc for the original.
+
+## Installation ##
+
+Add the following line to your composer.json file:
+
+    {
+        "require": {
+            "sharnw/filecached-php": "dev-master"
+        }
+    }
+
+## Usage ##
+
+Similar interface to memcache
+
+* `set(k, v)` set key-value pair
+* `get(k)` get key value, returns false if nothing present
+* `delete(k)` delete key-value pair
+* `flush()` wipe all cache data
+
+### Demo ###
+    
+    require_once('src/cache.php');
+
+    $cache = new Filecached\Cache();
+
+    $example_data = [
+        'time' => time(),
+        'message' => 'something certainly happened today.',
+    ];
+
+    $cache->set('todays_news', $example_data);
+
+    print_r($storeData = $cache->get('todays_news'));
+
+    $example_data = [
+        [
+            'url' => 'https://libraries.io/github/hustcc/php-file-cache',
+            'title' => 'Forked repo'
+        ],
+    ];
+
+    $cache->set('todays_links', $example_data);
+
+    print_r($storeData = $cache->get('todays_links'));
+
+    $cache->flush('today_'); // flush all data in 'today/' namespace
+
+    $cache->set('more data', ['blah']);
+
+    $cache->flush(); // flush all data
+
+## License ##
+Released under the terms of the [MIT license](http://opensource.org/licenses/MIT).
+
+## Links ##
+
+* Forked lib - https://libraries.io/github/hustcc/php-file-cache
---
+++ src/cache.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Filecached;
+
+/**
+*  
+* A simple key-value datastore using files instead of memory.
+*
+* Useful for caching large datasets, and replacing memcache on low memory AWS instances.
+* 
+* @author hustcc
+* @author sharnw
+* @link https://github.com/Sharnw/php-file-cache
+* @license MIT
+*/
+class Cache {
+
+    public $cache_path = 'cache/'; // default cache folder
+    public $cache_time = 86400; // cache file expires after 1 day
+    public $cache_extension = '.cache'; // default file extension
+    
+    public function __construct($cache_path = 'cache/', $cache_time = 86400, $cache_extension = '.cache') {
+        $this->cache_path = $cache_path;
+        $this->cache_time = $cache_time;
+        $this->cache_extension = $cache_extension;
+        if (!file_exists($this->cache_path)) {
+            mkdir($this->cache_path, 0777);
+        }
+    }
+    
+    /*
+    * Set a key/value pair
+    */
+    public function set($key, $value) {
+        if (empty($value)) $this->delete($key);
+
+        $filename = $this->_get_cache_file($key);
+        file_put_contents($filename, serialize($value));
+        //file_put_contents($filename, serialize($value), LOCK_EX);
+    }
+    
+    /*
+    * Remove a cache file by key
+    */
+    public function delete($key) {
+        $filename = $this->_get_cache_file($key);
+        if (file_exists($filename)) unlink($filename);
+    }
+    
+    /*
+    * Retrieve a cache file by key
+    */
+    public function get($key, $force_expire = false) {
+        if ($this->_has_cache($key, $force_expire)) {
+            $filename = $this->_get_cache_file($key);
+            $value = file_get_contents($filename);
+            if (empty($value)) {
+                return false;
+            }
+            return unserialize($value);
+        }
+    }
+    
+    /*
+    * Flush all cache files in a namespace, defaults to all
+    */
+    public function flush($prefix = '') {
+        $fp = opendir($this->cache_path);
+        while(!false == ($fn = readdir($fp))) {
+            if($fn == '.' || $fn =='..' || ($prefix != '' and substr($fn, 0, strlen($prefix)) != $prefix)) {
+                continue;
+            }
+            unlink($this->cache_path . $fn);
+        }
+    }
+
+    /*
+    * Remove all expired cache files
+    */
+    public function clean() {
+        $fp = opendir($this->cache_path);
+        while(!false == ($fn = readdir($fp))) {
+            if($fn == '.' || $fn =='..') {
+                continue;
+            }
+            if (filemtime($fn) + $this->cache_time >= time()) unlink($this->cache_path . $fn);
+        }
+    }
+    
+    /*
+    * Check for a chache file by key
+    *
+    * @param Integer $force_expire Pass a new timestamp for file to expire at
+    */
+    private function _has_cache($key, $force_expire = false) {
+        $expire_time = ($force_expire ? $force_expire : $this->cache_time);
+        $filename = $this->_get_cache_file($key);
+        if(file_exists($filename) && (filemtime($filename) + $expire_time >= time())) {
+            return true;
+        }
+        return false;
+    }
+    
+    /*
+    * Return a safe filename by key
+    */
+    private function _safe_filename($key) {
+        if ($key != null) {
+            $parts = explode('_', $key);
+            if (count($parts) > 1) { // allow for prefixing of filenames
+                return $parts[0].'_'.md5($key);
+            }
+            return md5($key);
+        }
+        return 'invalid_cache_key';
+    }
+    
+    /*
+    * Retrieve the expected cache file name by key
+    */
+    private function _get_cache_file($key) {
+        return $this->cache_path . $this->_safe_filename($key) . $this->cache_extension;
+    }
+}
+
+?>
